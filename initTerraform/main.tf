e88dc5c062e4172ab30b17a14139c954278562af
kevingonzalez7997 <59447523+kevingonzalez7997@users.noreply.github.com>
2023-10-27 21:14:58 -0400

    Update main.tf

--- initTerraform/main.tf
+++ initTerraform/main.tf
@@ -14,14 +14,14 @@ resource "aws_vpc" "deployment6_vpc_US_east" {
 #######################SUBNET##########################################
 resource "aws_subnet" "public_1" {
     cidr_block = "10.0.1.0/24"
-    vpc_id = aws_vpc.deployment6-vpc-USeast.id
+    vpc_id = aws_vpc.deployment6_vpc_US_east.id
     availability_zone = "us-east-1a"
     map_public_ip_on_launch = true  
 }
 
 resource "aws_subnet" "public_2" {
     cidr_block = "10.0.2.0/24"
-    vpc_id = aws_vpc.deployment6-vpc-USeast.id
+    vpc_id = aws_vpc.deployment6_vpc_US_east.id
     availability_zone = "us-east-1b"
     map_public_ip_on_launch = true  
 }
@@ -30,7 +30,7 @@ resource "aws_subnet" "public_2" {
 resource "aws_security_group" "pub1_sercurity" {
   name ="app_and_ssh"
   description = "pub1_sercurity"
-  vpc_id = aws_vpc.deployment6-vpc-USeast.id
+  vpc_id = aws_vpc.deployment6_vpc_US_east.id
   
   ingress {
     from_port = 22
@@ -55,7 +55,7 @@ resource "aws_security_group" "pub1_sercurity" {
 
 }
 #####################################EC2#################################################
-resource "aws_instance" "app_ec2_1" {
+resource "aws_instance" "bankapp" {
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.medium"
   availability_zone = "us-east-1b" # Specify the desired availability zone
@@ -68,20 +68,22 @@ resource "aws_instance" "app_ec2_1" {
   }
 }
 
-resource "aws_instance" "app_ec2_2" {
+resource "aws_instance" "bankapp2" {
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.medium"
   availability_zone = "us-east-1b" # Specify the desired availability zone
   subnet_id = aws_subnet.public_2.id
   vpc_security_group_ids = [aws_security_group.pub1_sercurity.id]
   key_name = "deploy_6"
+  user_data = "${file("bankapp_install.sh")}"
+
   tags = {
     Name = "App_ec2_2"
   }
 }
 #######################IGW##################################################################
 resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.d5-1_vpc.id
+  vpc_id = aws_vpc.deployment6_vpc_US_east.id
 
   tags = {
     Name = "gw_d6"
@@ -90,7 +92,7 @@ resource "aws_internet_gateway" "gw" {
 
 ##############################################################################################
 resource "aws_default_route_table" "route_d6" {
-  default_route_table_id = aws_vpc.d5-1_vpc.default_route_table_id
+  default_route_table_id = aws_vpc.deployment6_vpc_US_east.default_route_table_id
    route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
