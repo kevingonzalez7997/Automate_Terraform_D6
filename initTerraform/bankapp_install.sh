20907b7b0086e4e0f16f2000bf4ceb9bbe2500db
kevingonzalez7997 <59447523+kevingonzalez7997@users.noreply.github.com>
2023-10-27 22:51:20 -0400

    Update bankapp_install.sh

--- initTerraform/bankapp_install.sh
+++ initTerraform/bankapp_install.sh
@@ -17,10 +17,8 @@ cd Automate_Terraform_D6
 pip install -r requirements.txt
 pip install mysqlclient
 pip install gunicorn
-pip install mysqlclient
 python database.py
 sleep 1
 python load_data.py
 sleep 1 
-python -m gunicorn app:app -b 0.0.0.0 -D 
-source test/bin/activate
+python -m gunicorn app:app -b 0.0.0.0 -D
