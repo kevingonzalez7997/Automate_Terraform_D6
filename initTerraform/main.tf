4fcc9c9d22d777acbfcee807ef73084273f98146
kevingonzalez7997 <59447523+kevingonzalez7997@users.noreply.github.com>
2023-10-28 09:33:37 -0400

    Update main.tf

--- initTerraform/main.tf
+++ initTerraform/main.tf
@@ -5,16 +5,16 @@ provider "aws" {
   region = "us-west-1"
 }
 #########################VPC#########################################
-resource "aws_vpc" "deployment6_vpc_US_west" {
+resource "aws_vpc" "D6_vpc_US_west" {
   cidr_block       = "10.0.0.0/16"
   tags = {
-    Name = "deployment6-vpc-US-west"
+    Name = "D6_vpc_US_west"
   }
 }
 #######################SUBNET##########################################
 resource "aws_subnet" "public_1" {
     cidr_block = "10.0.1.0/24"
-    vpc_id = aws_vpc.deployment6_vpc_US_west.id
+    vpc_id = aws_vpc.D6_vpc_US_west.id
     availability_zone = "us-west-1a"
     map_public_ip_on_launch = true  
     tags = {
@@ -25,20 +25,19 @@ resource "aws_subnet" "public_1" {
 
 resource "aws_subnet" "public_2" {
     cidr_block = "10.0.2.0/24"
-    vpc_id = aws_vpc.deployment6_vpc_US_west.id
-    availability_zone = "us-west-1c"
+    vpc_id = aws_vpc.D6_vpc_US_west.id
+    availability_zone = "us-west-1b"
     map_public_ip_on_launch = true  
     tags = {
     Name = "PublicSubnet_West2"
  }
     
 }
-
 ####################SECURITY_GROUP#######################################
-resource "aws_security_group" "pub1_sercurity" {
+resource "aws_security_group" "pub_sercurity_west" {
   name ="app_and_ssh"
-  description = "pub1_sercurity"
-  vpc_id = aws_vpc.deployment6_vpc_US_west.id
+  description = "pub_sercurity_west"
+  vpc_id = aws_vpc.D6_vpc_US_west.id
   
   ingress {
     from_port = 22
@@ -73,7 +72,7 @@ resource "aws_instance" "bankapp" {
   ami           = "ami-0cbd40f694b804622"
   instance_type = "t2.medium"
   subnet_id = aws_subnet.public_1.id
-  vpc_security_group_ids = [aws_security_group.pub1_sercurity.id]
+  vpc_security_group_ids = [aws_security_group.pub_sercurity_west.id]
   key_name = "D6keyWest"
   user_data = "${file("appsetup.sh")}"
   tags = {
@@ -85,7 +84,7 @@ resource "aws_instance" "bankapp2" {
   ami           = "ami-0cbd40f694b804622"
   instance_type = "t2.medium"
   subnet_id = aws_subnet.public_2.id
-  vpc_security_group_ids = [aws_security_group.pub1_sercurity.id]
+  vpc_security_group_ids = [aws_security_group.pub_sercurity_west.id]
   key_name = "D6keyWest"
   user_data = "${file("appsetup.sh")}"
 
@@ -95,7 +94,7 @@ resource "aws_instance" "bankapp2" {
 }
 #######################IGW##################################################################
 resource "aws_internet_gateway" "gw_west" {
-  vpc_id = aws_vpc.deployment6_vpc_US_west.id
+  vpc_id = aws_vpc.D6_vpc_US_west.id
 
   tags = {
     Name = "gw_d6_west"
@@ -104,7 +103,7 @@ resource "aws_internet_gateway" "gw_west" {
 
 ##############################################################################################
 resource "aws_default_route_table" "routed6_west" {
-  default_route_table_id = aws_vpc.deployment6_vpc_US_west.default_route_table_id
+  default_route_table_id = aws_vpc.D6_vpc_US_west.default_route_table_id
    route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw_west.id
