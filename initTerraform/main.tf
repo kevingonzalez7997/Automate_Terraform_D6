f88da426863d945a96188d71a556ef3d8b84e9a6
kevingonzalez7997 <59447523+kevingonzalez7997@users.noreply.github.com>
2023-10-28 20:33:39 -0400

    Update main.tf

--- initTerraform/main.tf
+++ initTerraform/main.tf
@@ -94,3 +94,78 @@ resource "aws_default_route_table" "D6_route_east" {
     gateway_id = aws_internet_gateway.D6Eastgw.id
   }
 }
+
+
+
+
+
+######################### TARGET GROUP CREATION ######################################
+## Create a target group for the load balancer to use
+## port 8000 is used for our application 
+resource "aws_lb_target_group" "app_backups_ec2" {
+  name = "AppBackupsEC2"
+  port = 8000
+  protocol = "HTTP"
+  vpc_id = aws_vpc.D6_vpc_us_east.id
+}
+#################### EC2 ATTACHMENT TO TARGET GROUP ###########################################
+## Once the target group resource has been created
+## attach each agent ec2 -> target group
+#AmazonResourceName
+resource "aws_lb_target_group_attachment" "register_target_1" {
+  target_group_arn = aws_lb_target_group.app_backups_ec2.arn
+  target_id = aws_instance.bankapp.id
+  port = 8000
+}
+
+resource "aws_lb_target_group_attachment" "register_target_2" {
+  target_group_arn = aws_lb_target_group.app_backups_ec2.arn
+  target_id = aws_instance.bankapp2.id
+  port = 8000
+}
+
+
+###################### LOADBALANCER  ###################################################
+resource "aws_lb" "my_load_balancer" {
+  name               = "d6LoadBalanceEast"
+  internal           = false # Set to true for internal ALB
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.load_balancer_sg.id]
+  subnets            = [aws_subnet.public_subnet1_east.id, aws_subnet.public_subnet2_east.id]
+}
+
+
+############################# LISTNER ###########################################
+#Each load balancer needs a listener to accept incoming requests
+resource "aws_lb_listener" "traffichandler" {
+  load_balancer_arn = aws_lb.my_load_balancer.arn
+  port              = "80"
+  protocol          = "HTTP"
+ default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_backups_ec2.arn
+  }
+}
+
+###############################LB SECURITY GROUP###############################
+resource "aws_security_group" "load_balancer_sg" {
+  name        = "d6East-Load-balancer-sg"
+  description = "Security group for the load balancer"
+  vpc_id      = aws_vpc.D6_vpc_us_east.id
+
+# Since the application is accessed through 8000 
+# The Load balancer needs to be able to accept traffic from that port 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
